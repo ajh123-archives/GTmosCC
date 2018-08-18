746bb4d29b257882c00a6be02aea51a89a10a186
ajh123 <41990982+ajh123@users.noreply.github.com>
2018-08-18 16:05:18 +0100

    Add files via upload

---
+++ startup.lua
@@ -0,0 +1,29 @@
+
+term.clear()
+term.setCursorPos(1,1)
+print("Botting GT-MOS")
+
+sleep(0.5)
+
+term.setCursorPos(1,1)
+print("   ...000...   ")
+sleep(0.5)
+    
+term.setCursorPos(1,1)
+print("   ..00.00..   ")
+sleep(0.5)
+
+term.setCursorPos(1,1)
+print("   .00...00.   ")
+sleep(0.5)
+    
+term.setCursorPos(1,1)
+print("   ..00.00..   ")
+sleep(0.5)
+
+term.setCursorPos(1,1)
+print("   ...000...   ")
+sleep(0.5)
+    
+
+    
